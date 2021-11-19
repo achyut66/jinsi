3a77219571fa8524dfda7175a618afcdeac6dc7e
Achyut Neupane <green.band66@gmail.com>
2021-11-19 16:02:24 +0545

    jinsi system locally setup for demo

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(0);
+	defined("DB_SERVER")? 	NULL : 	define("DB_SERVER", "localhost");
+	defined("DB_USER") ? 	NULL : 	define("DB_USER", "sagarlam_jinsib");
+	defined("DB_PASS") ? 	NULL : 	define("DB_PASS", "bakaiyajinsi78");
+	defined("DB_NAME") ? 	NULL : 	define("DB_NAME", "sagarlam_bakaiya_jinsi");
+?>
---
+++ includes/defines.php
@@ -0,0 +1,15 @@
+<?php
+defined("DS")? NULL : define('DS','/');
+defined("KEYID")? NULL : define('KEYID','669d55221cf323ee455e8e94b4840d1cjwalamukhi_auth_id');
+defined("KEYMODE")? NULL : define('KEYMODE','669d55221cf323ee455e8e94b4840d1cjwalamukhi_auth_id_mode');
+defined("SITE_ROOT")? NULL : define('SITE_ROOT',$_SERVER['DOCUMENT_ROOT'].DS.'bakaiya/jinsi'.DS);
+defined("LIB_INCLUDE")? NULL : define('LIB_INCLUDE',SITE_ROOT.'includes'.DS);
+defined("MENU_INCLUDES")? NULL : define('MENU_INCLUDES',SITE_ROOT.'menuincludes'.DS);
+defined("SITE_NAME")? NULL : define('SITE_NAME','बकैया गाउँपालिका');
+defined("SITE_TYPE")? NULL : define('SITE_TYPE','गाउँपालिका ');
+defined("SITE_ADDRESS")? NULL : define('SITE_ADDRESS','हात्तीसुँडे, मकवानपुर');
+defined("SITE_SUBHEADING")? NULL : define('SITE_SUBHEADING','Bakaiya Rural Municipality');
+defined("SITE_LOCATION")?NULL:define('SITE_LOCATION','गाउँ कार्यपालिकाको कार्यालय ');
+defined("OFFICE_SITE")? NULL : define('OFFICE_SITE','http://bmsnep.com/');
+defined("OFFICE_NAME")? NULL : define('OFFICE_NAME','BMS');
+?>
